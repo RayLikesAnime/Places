e9df8588addaca1233b58457c95db2c5f9e500bb
Keshav Pandey <88835211+RayLikesAnime@users.noreply.github.com>
2024-05-14 21:53:22 +0530

    basic website

---
+++ places/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+
+function App() {
+  return (
+    <Router>
+      <MainNavigation />
+      <main>
+      <Routes>
+        <Route path="/" element={<Users />} />
+        <Route path="/places/new" element={<NewPlace/>} />
+
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+      </main>
+    </Router>
+  );
+}
+
+export default App;
