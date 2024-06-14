76f136348105fbf7b2a92c4918a589b005447b63
Keshav Pandey <88835211+RayLikesAnime@users.noreply.github.com>
2024-06-15 00:36:37 +0530

    fetch users and create place works

--- places/src/places/pages/NewPlace.js
+++ places/src/places/pages/NewPlace.js
@@ -1,11 +1,21 @@
-import React,{act, useCallback, useReducer} from "react";
+import React,{act, useCallback, useReducer,useContext} from "react";
 import './NewPlace.css';
+import {Link, useNavigate} from 'react-router-dom';
 import Input from "../../shared/components/FormElements/Input";
 import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../shared/util/validators";
 import Button from "../../shared/components/FormElements/Button";
 import { useForm } from "../../shared/hooks/form-hook";
+import {useHttpClient} from '../../shared/hooks/http-hook'
+import { AuthContext } from "../../shared/context/auth-context";
+import ErrorModal from '../../shared/components/UIElements/ErrorModal'
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+
 
 const NewPlace = () => {
+
+    const auth=useContext(AuthContext);
+
+    const {isLoading,error,sendRequest,clearError}=useHttpClient();
 
     const [formState,inputHandler]=useForm({
         title: {
@@ -22,20 +32,40 @@ const NewPlace = () => {
         }
     }, false);
 
-    const placeSubmitHandler = event => {
+    const navigate=useNavigate();
+
+    const placeSubmitHandler =async event => {
         event.preventDefault();
-        console.log(formState.inputs);
+        try{
+            await sendRequest('http://localhost:5000/api/places','POST',JSON.stringify({
+            title: formState.inputs.title.value,
+            description: formState.inputs.description.value,
+            address: formState.inputs.address.value,
+            creator: auth.userId
+        }),
+        {
+            'Content-Type': 'application/json'
+        }
+        );
+        navigate('/');
+        }catch(err){
+            console.log(err.message);
+        }
     };
 
 
 
     return (
+        <>
+        <ErrorModal error={error} onClear={clearError} />
     <form className="place-form" onSubmit={placeSubmitHandler}>
+        {isLoading && <LoadingSpinner asOverlay />}
         <Input id="title" element="input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a valid title." onInput={inputHandler} />
         <Input id="description" element="textarea" label="Description" validators={[VALIDATOR_MINLENGTH(5)]} errorText="Please enter a valid description(atleast 5 characters)." onInput={inputHandler} />
         <Input id="address" element="input" label="Address" validators={[VALIDATOR_REQUIRE]} errorText="Please enter a valid address." onInput={inputHandler} />
         <Button type="submit" disabled={!formState.isValid}>ADD PLACE</Button>
     </form>
+    </>
     );
 }
 
