b711856bf8a8035415e553639bb2db5c7d6dc908
Keshav Pandey <88835211+RayLikesAnime@users.noreply.github.com>
2024-06-16 03:43:23 +0530

    image upload integrated

--- places/src/places/pages/NewPlace.js
+++ places/src/places/pages/NewPlace.js
@@ -9,6 +9,7 @@ import {useHttpClient} from '../../shared/hooks/http-hook'
 import { AuthContext } from "../../shared/context/auth-context";
 import ErrorModal from '../../shared/components/UIElements/ErrorModal'
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import ImageUpload from "../../shared/components/FormElements/imageUpload";
 
 
 const NewPlace = () => {
@@ -29,6 +30,10 @@ const NewPlace = () => {
         address: {
             value: '',
             isValid: false
+        },
+        image:{
+            value: null,
+            isValid: false
         }
     }, false);
 
@@ -37,16 +42,14 @@ const NewPlace = () => {
     const placeSubmitHandler =async event => {
         event.preventDefault();
         try{
-            await sendRequest('http://localhost:5000/api/places','POST',JSON.stringify({
-            title: formState.inputs.title.value,
-            description: formState.inputs.description.value,
-            address: formState.inputs.address.value,
-            creator: auth.userId
-        }),
-        {
-            'Content-Type': 'application/json'
-        }
-        );
+        const formData=new FormData();
+        formData.append('title',formState.inputs.title.value);
+        formData.append('description',formState.inputs.description.value);
+        formData.append('address',formState.inputs.address.value);
+        formData.append('creator',auth.userId);
+        formData.append('image',formState.inputs.image.value);
+
+            await sendRequest('http://localhost:5000/api/places','POST',formData);
         navigate('/');
         }catch(err){
             console.log(err.message);
@@ -63,6 +66,7 @@ const NewPlace = () => {
         <Input id="title" element="input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a valid title." onInput={inputHandler} />
         <Input id="description" element="textarea" label="Description" validators={[VALIDATOR_MINLENGTH(5)]} errorText="Please enter a valid description(atleast 5 characters)." onInput={inputHandler} />
         <Input id="address" element="input" label="Address" validators={[VALIDATOR_REQUIRE]} errorText="Please enter a valid address." onInput={inputHandler} />
+        <ImageUpload id="image" onInput={inputHandler} errorText="PLease provide an image" />
         <Button type="submit" disabled={!formState.isValid}>ADD PLACE</Button>
     </form>
     </>
